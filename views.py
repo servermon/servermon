2d93330342e1562e4b796eee13f0fef04fcfb4ec
Faidon Liambotis <faidon@noc.grnet.gr>
2012-02-29 17:37:47 +0200

    Get rid of obsolete virt/ app

--- views.py
+++ views.py
@@ -1,5 +1,4 @@
 from puppet.models import Host, Fact, FactValue
-from virt.models import Domain, Cluster, Node
 from updates.models import *
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
@@ -12,112 +11,68 @@ from IPy import IP
 import re
 
 def host(request,hostname):
-    host = None
-    isvm = False
-    ispuppet = False
-    vmdisks = []
-    vmifaces = []
+    host = get_object_or_404(Host, name=hostname)
+
     updates = []
-    vms = []
     system = []
-    contacts = []
-    node = None
-    # First try and see if this is a puppet host
-    try:
-        host = Host.objects.get(name=hostname)
-        ispuppet = True
-    except:
-        pass
 
-    if host:
-        for vm in host.domain_set.all():
-            isvm = True
-            vmdisks = vm.get_disks()
-            vmifaces = vm.get_interfaces()
-            break
-        
-    
-    else:
+    iflist = host.factvalue_set.get(fact_name__name='interfaces').value.split(',')
+    interfaces = []
+    for iface in iflist:
+        d = {}
+        iface1 = iface.replace(':','_')
         try:
-            host = Domain.objects.get(puppet_host__isnull=True, name=hostname)
-            vmdisks = host.get_disks()
-            vmifaces = host.get_interfaces()
-            isvm = True
+            mac = host.factvalue_set.get(fact_name__name='macaddress_%s' % iface1).value
         except:
-            raise Http404
+            mac = None
+        try:
+            ip = host.factvalue_set.get(fact_name__name='ipaddress_%s' % iface1).value
+            netmask = host.factvalue_set.get(fact_name__name='netmask_%s' % iface1).value
+        except:
+            ip = None
+            netmask = None
 
+        d = { 'iface': iface,
+              'mac': mac }
 
-    if ispuppet:
-        iflist = host.factvalue_set.get(fact_name__name='interfaces').value.split(',')
-        interfaces = []
-        for iface in iflist:
-            d = {}
-            iface1 = iface.replace(':','_')
-            try:
-                mac = host.factvalue_set.get(fact_name__name='macaddress_%s' % iface1).value
-            except:
-                mac = None
-            try:
-                ip = host.factvalue_set.get(fact_name__name='ipaddress_%s' % iface1).value
-                netmask = host.factvalue_set.get(fact_name__name='netmask_%s' % iface1).value
-            except:
-                ip = None
-                netmask = None
+        interfaces.append(d)
 
-            d = { 'iface': iface,
-                  'mac': mac }
+        if netmask and ip:
+            interfaces[-1]['ipaddr'] = "%s/%d" % (ip, IP(ip).make_net(netmask).prefixlen())
 
-            for vmif in filter(lambda x: x['mac'] == d['mac'], vmifaces):
-                try:
-                    d['bridge'] = vmif['bridge']
-                    d['hostif'] = vmif['name']
-                except:
-                    pass
-                break
+    system = []
+    for fact, label in [
+        ('bios_date', 'BIOS Date'),  ('bios_version', 'BIOS Version'),
+        ('manufacturer', 'System Vendor'), ('productname', 'Model'),
+        ('serialnumber', 'Serial Nr'), ('processorcount', 'Processors'),
+        ('architecture', 'Architecture'), ('virtual', 'Machine Type')]:
+        system.append({ 'name': label, 'value': host.get_fact_value(fact) })
 
-            interfaces.append(d)
+    system.append({ 'name': 'Processor type',
+        'value': ", ".join([ p['value'] for p in host.factvalue_set.filter(fact_name__name__startswith='processor').exclude(fact_name__name='processorcount').values('value').distinct() ]),
+    })
 
-            if netmask and ip:
-                interfaces[-1]['ipaddr'] = "%s/%d" % (ip, IP(ip).make_net(netmask).prefixlen())
-            
-        system = []
-        for fact, label in [
-            ('bios_date', 'BIOS Date'),  ('bios_version', 'BIOS Version'),
-            ('manufacturer', 'System Vendor'), ('productname', 'Model'),
-            ('serialnumber', 'Serial Nr'), ('processorcount', 'Processors'),
-            ('architecture', 'Architecture'), ('virtual', 'Machine Type')]:
-            system.append({ 'name': label, 'value': host.get_fact_value(fact) })
-
-        system.append({ 'name': 'Processor type',
-            'value': ", ".join([ p['value'] for p in host.factvalue_set.filter(fact_name__name__startswith='processor').exclude(fact_name__name='processorcount').values('value').distinct() ]),
+    system.append({ 'name': 'Operating System',
+        'value': "%s %s" % (host.get_fact_value('operatingsystem'), host.get_fact_value('operatingsystemrelease'))
+        })
+    system.append({ 'name': 'Memory',
+        'value': "%s (%s free)" % (host.get_fact_value('memorysize'), host.get_fact_value('memoryfree'))
         })
 
-        system.append({ 'name': 'Operating System',
-            'value': "%s %s" % (host.get_fact_value('operatingsystem'), host.get_fact_value('operatingsystemrelease'))
-            })
-        system.append({ 'name': 'Memory',
-            'value': "%s (%s free)" % (host.get_fact_value('memorysize'), host.get_fact_value('memoryfree'))
-            })
+    system.append({ 'name': 'Puppet classes',
+        'value': ", ".join([ f.value for f in  host.factvalue_set.filter(fact_name__name='puppetclass') ])
+        })
 
-        system.append({ 'name': 'Puppet classes',
-            'value': ", ".join([ f.value for f in  host.factvalue_set.filter(fact_name__name='puppetclass') ])
-            })
+    #return render_to_response("host.html", { 'host': host, 'interfaces': interfaces, 'system': system })
 
-        #return render_to_response("host.html", { 'host': host, 'interfaces': interfaces, 'system': system })
+    updates = host.update_set.order_by('package__name')
 
-        updates = host.update_set.order_by('package__name')
-
-        if host.node_set.all():
-            node = host.node_set.all()[0]
-            vms =  Domain.objects.filter(node=node)
-            for vm in vms:
-                contacts.extend([ { 'contact': c, 'vm': vm } for c in vm.domaincontact_set.all() ])
-            contacts.sort(cmp=lambda x,y: cmp(x['contact'].name, y['contact'].name))
-
-    else:
-        interfaces = vmifaces
-
-    return render_to_response('hostview.html', {'host': host, 'updates': updates, 'interfaces': interfaces, 'system': system, 'vms': vms, 'contacts': contacts, 'node': node, 'vmdisks': vmdisks, 'isvm': isvm, 'ispuppet': ispuppet })
+    return render_to_response('hostview.html', {
+        'host': host,
+        'updates': updates,
+        'interfaces': interfaces,
+        'system': system,
+        })
 
 
 def inventory(request):
@@ -146,22 +101,14 @@ def index(request):
     factvaluecount = FactValue.objects.count()
     updatecount = Host.objects.filter(package__isnull=False).distinct().count()
     packagecount = Package.objects.count()
-    problemvms = Domain.objects.filter(last_seen__lte=(datetime.now() - timedelta(seconds=VM_TIMEOUT))).order_by('name')
-    vmcount = Domain.objects.count()
-    clustercount = Cluster.objects.count()
-    nodecount = Node.objects.count()
 
     return render_to_response("index.html", {
         'problemhosts': problemhosts, 
-        'problemvms': problemvms,
         'hosts': hosts,
         'factcount': factcount,
         'factvaluecount': factvaluecount,
         'updatecount': updatecount,
         'packagecount': packagecount,
-        'vmcount': vmcount,
-        'clustercount': clustercount,
-        'nodecount': nodecount,
         })
 
 def search(request):
@@ -176,9 +123,7 @@ def search(request):
         for r in res:
             matches.append({'name': r.host.name, 'attribute': name, 'value': regex.sub(r'<strong>\1</strong>', r.value)})
 
-    vms = Domain.objects.filter(name__icontains=request.POST['search'])
-
-    return render_to_response("search.html", {'matches': matches , 'search': request.POST['search'], 'vms': vms })
+    return render_to_response("search.html", {'matches': matches , 'search': request.POST['search']})
 
 
 def query(request):
