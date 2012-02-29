5ee053b8e71aace3a1ea4fad1723318065e8b1bc
Faidon Liambotis <faidon@noc.grnet.gr>
2012-02-29 17:00:33 +0200

    Add text/plain output for package updates

--- updates/views.py
+++ updates/views.py
@@ -19,4 +19,6 @@ def host(request,hostname):
 def package(request,packagename):
     package = Package.objects.filter(name=packagename)[0]
     updates = package.update_set.order_by('host__name')
+    if "plain" in request.GET:
+        return render_to_response("package.txt", {"updates": updates}, mimetype="text/plain")
     return render_to_response('packageview.html', {'package': package, 'updates': updates})
