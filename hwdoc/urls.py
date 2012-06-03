559e2f7e8d3efd34623364b95cec25df9b17e16b
Alexandros Kosiaris <alex@noc.grnet.gr>
2012-06-03 10:01:26 +0100

    URLConf docstring
    This refs #2428

--- hwdoc/urls.py
+++ hwdoc/urls.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*- vim:encoding=utf-8:
 # vim: tabstop=4:shiftwidth=4:softtabstop=4:expandtab
+'''
+Module containing hwdoc URLconf
+'''
+
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('servermon.hwdoc.views',
