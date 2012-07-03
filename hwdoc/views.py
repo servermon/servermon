servermon/servermon

commit dfe3da92656f563ea6aef6bb11920137aa04a366

Views, URLconf and dummy templates for new models
This commit adds views, URLconf and some dummy templates for Rack, RackRow and
Datacenter.

hwdoc/views.py
@@ -77,6 +77,51 @@ def project(request, project_id):
     project = get_object_or_404(Project,pk=project_id)
     return render(request, template, { 'project': project, })
 
+def rack(request, rack_id):
+    '''
+    Rack view. It should display all non-authenticated user viewable data
+
+    @type   request: HTTPRequest 
+    @param  request: Django HTTPRequest object
+    @rtype: HTTPResponse
+    @return: HTTPResponse object rendering corresponding HTML
+    '''
+
+    template = 'rack.html'
+
+    rack = get_object_or_404(Rack, pk=rack_id)
+    return render(request, template, { 'rack': rack, })
+
+def rackrow(request, rackrow_id):
+    '''
+    Rackrow view. It should display all non-authenticated user viewable data
+
+    @type   request: HTTPRequest 
+    @param  request: Django HTTPRequest object
+    @rtype: HTTPResponse
+    @return: HTTPResponse object rendering corresponding HTML
+    '''
+
+    template = 'rackrow.html'
+
+    rack = get_object_or_404(RackRow, pk=rackrow_id)
+    return render(request, template, { 'rackrow': rackrow, })
+
+def datacenter(request, datacenter_id):
+    '''
+    Rackrow view. It should display all non-authenticated user viewable data
+
+    @type   request: HTTPRequest 
+    @param  request: Django HTTPRequest object
+    @rtype: HTTPResponse
+    @return: HTTPResponse object rendering corresponding HTML
+    '''
+
+    template = 'datacenter.html'
+
+    rack = get_object_or_404(Datacenter, pk=datacenter_id)
+    return render(request, template, { 'datacenter': datacenter, })
+
 def search(request):
     '''
     Search view. Scans request for q (GET case) or qarea (POST case) and

hwdoc/urls.py
@@ -10,6 +10,9 @@ urlpatterns = patterns('servermon.hwdoc.views',
             (r'^$', 'index'),
             (r'^project/(?P<project_id>[\d]+)/$', 'project'),
             (r'^equipment/(?P<equipment_id>[\d]+)/$', 'equipment'),
+            (r'^rack/(?P<rack_id>[\d]+)/$', 'rack'),
+            (r'^rackrow/(?P<rackrow_id>[\d]+)/$', 'rackrow'),
+            (r'^datacenter/(?P<datacenter_id>[\d]+)/$', 'datacenter'),
             (r'^search/$', 'search'),
             (r'^advancedsearch/$', 'advancedsearch'),
             # Opensearch

commit 17fb06de235fa4774523b2de0874c5ac6c56ce6c

Bugs by omissions in hwdoc/views fixed
This commit fixes a number of bugs introduced by
dfe3da92656f563ea6aef6bb11920137aa04a366 by importing needed models and
fixing a number of typos. In the same commit we make sure we pass some
extra needed info to rackrow template

hwdoc/views.py
@@ -18,7 +18,8 @@
 hwdoc views module
 '''
 
-from servermon.hwdoc.models import Project, EquipmentModel, Equipment, ServerManagement, Rack
+from servermon.hwdoc.models import Project, EquipmentModel, Equipment, \
+        ServerManagement, Rack, RackRow, Datacenter
 from servermon.hwdoc import functions
 from servermon.compat import render
 from django.shortcuts import get_object_or_404
@@ -104,8 +105,13 @@ def rackrow(request, rackrow_id):
 
     template = 'rackrow.html'
 
-    rack = get_object_or_404(RackRow, pk=rackrow_id)
-    return render(request, template, { 'rackrow': rackrow, })
+    rackrow = get_object_or_404(RackRow, pk=rackrow_id)
+    racks = rackrow.rackposition_set.values_list('rack')
+    Us = Equipment.objects.filter(rack__in=racks).values_list('unit', flat=True).distinct()
+    return render(request, template, {
+        'rackrow': rackrow,
+        'Us': Us,
+        })
 
 def datacenter(request, datacenter_id):
     '''
@@ -119,7 +125,7 @@ def datacenter(request, datacenter_id):
 
     template = 'datacenter.html'
 
-    rack = get_object_or_404(Datacenter, pk=datacenter_id)
+    datacenter = get_object_or_404(Datacenter, pk=datacenter_id)
     return render(request, template, { 'datacenter': datacenter, })
 
 def search(request):