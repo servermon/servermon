5374eb78dd1d71b56fc6afe76cca9efeb9ab61ba
Alexandros Kosiaris <alex@noc.grnet.gr>
2012-10-29 15:20:52 +0200

    Fix inspection left outs
    Inspection of puppet database left some garbage with relations. Fix that
    Also some reordering of models was necessary

--- puppet/models.py
+++ puppet/models.py
@@ -18,6 +18,15 @@
 
 from django.db import models
 
+class SourceFile(models.Model):
+    filename = models.CharField(max_length=255, blank=True)
+    path = models.CharField(max_length=255, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    class Meta:
+        db_table = u'source_files'
+        managed = False
+
 class Fact(models.Model):
     name = models.CharField(max_length=255)
     updated_at = models.DateTimeField(null=True, blank=True)
@@ -38,7 +47,7 @@ class Host(models.Model):
     last_freshcheck = models.DateTimeField(null=True, blank=True)
     last_report = models.DateTimeField(null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
-    source_file_id = models.IntegerField(null=True, blank=True)
+    source_file = models.ForeignKey(SourceFile, null=True, blank=True)
     created_at = models.DateTimeField(null=True, blank=True)
     facts = models.ManyToManyField(Fact, through='FactValue')
 
@@ -56,6 +65,18 @@ class Host(models.Model):
         except:
             return default
 
+class Resource(models.Model):
+    title = models.TextField()
+    restype = models.CharField(max_length=255)
+    host = models.ForeignKey(Host, null=True, blank=True)
+    source_file = models.ForeignKey(SourceFile, null=True, blank=True)
+    exported = models.IntegerField(null=True, blank=True)
+    line = models.IntegerField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    class Meta:
+        db_table = u'resources'
+        managed = False
 
 class FactValue(models.Model):
     value = models.TextField()
@@ -87,9 +108,9 @@ class ParamNames(models.Model):
 
 class ParamValues(models.Model):
     value = models.TextField()
-    param_name_id = models.IntegerField()
+    param_name = models.ForeignKey(ParamNames)
     line = models.IntegerField(null=True, blank=True)
-    resource_id = models.IntegerField(null=True, blank=True)
+    resource = models.ForeignKey(Resource, null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(null=True, blank=True)
     class Meta:
@@ -105,33 +126,11 @@ class PuppetTags(models.Model):
         managed = False
 
 class ResourceTags(models.Model):
-    resource_id = models.IntegerField(null=True, blank=True)
-    puppet_tag_id = models.IntegerField(null=True, blank=True)
+    resource = models.ForeignKey(Resource, null=True, blank=True)
+    puppet_tag = models.ForeignKey(PuppetTags, null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(null=True, blank=True)
     class Meta:
         db_table = u'resource_tags'
-        managed = False
-
-class SourceFile(models.Model):
-    filename = models.CharField(max_length=255, blank=True)
-    path = models.CharField(max_length=255, blank=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(null=True, blank=True)
-    class Meta:
-        db_table = u'source_files'
-        managed = False
-
-class Resource(models.Model):
-    title = models.TextField()
-    restype = models.CharField(max_length=255)
-    host = models.ForeignKey(Host, null=True, blank=True)
-    source_file = models.ForeignKey(SourceFile, null=True, blank=True)
-    exported = models.IntegerField(null=True, blank=True)
-    line = models.IntegerField(null=True, blank=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(null=True, blank=True)
-    class Meta:
-        db_table = u'resources'
         managed = False
 
