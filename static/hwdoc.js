6f3f669ab04e638af61830f23f58ee537e23f0b8
Alexandros Kosiaris <alex@noc.grnet.gr>
2013-04-20 11:33:37 +0300

    Populate submenu hrefs
    Using c711f8a269dc2b7200459d1ae8d43ec59a294218 changes we populate
    dropdown submenu's items' hrefs

--- static/hwdoc.js
+++ static/hwdoc.js
@@ -1,9 +1,10 @@
 function fetch_and_update(obj, url) {
+  var hwdocurl = $(obj).children('a').attr('href').replace('1/','');
   $.get(url + obj.id,
   function(data) {
     var r = '';
     for (item in data) {
-      r = r + '<li><a href="#">' + data[item].fields.name + '</a></li>';
+      r = r + '<li><a href="' + hwdocurl + data[item].pk + '">' + data[item].fields.name + '</a></li>';
     }
     $(obj).children('ul').html(r);
   });
