87f24d42af59e12b6dac812427546bdedc6fbbc9
Rowan Thorpe <rowan@rowanthorpe.com>
2014-07-25 00:07:10 +0300

    Improve jira integration module
    * set unique=True for Ticket name field to prevent race conditions (helps
      all ticketing modules, not just jira)
    * use update_or_create() instead of blindly creating new tickets
    * allow to filter search by project(s) or not
    * implement basic and oauth authentication

--- servermon/hwdoc/vendor/ticket_jira.py
+++ servermon/hwdoc/vendor/ticket_jira.py
@@ -9,27 +9,48 @@ from django.conf import settings
 from jira.client import JIRA
 
 options = {
-    'server': settings.JIRA_TICKETING_URL
+    'server': settings.JIRA_TICKETING['url']
 }
+if settings.JIRA_TICKETING['auth']['type'] == 'oauth':
+    key_cert_data = None
+    with open(settings.JIRA_TICKETING['auth']['key_cert'], 'r') as key_cert_file:
+        key_cert_data = key_cert_file.read()
+    options.update(oauth={
+        'access_token': settings.JIRA_TICKETING['auth']['access_token'],
+        'access_token_secret': settings.JIRA_TICKETING['auth']['access_token_secret'],
+        'consumer_key': settings.JIRA_TICKETING['auth']['consumer_key'],
+        'key_cert': key_cert_data
+    })
+elif settings.JIRA_TICKETING['auth']['type'] == 'basic':
+    options.update(basic_auth=(
+        settings.JIRA_TICKETING['auth']['user'],
+        settings.JIRA_TICKETING['auth']['password'],
+    ))
+
 jira = JIRA(options)
 
 def get_tickets(equipment, closed):
     '''
     Populate tickets for equipment
     '''
-    if closed:
-        issues = [issue for issue in jira.search_issues('text ~ "' + str(equipment.serial) + '"')]
-    else:
-        issues = [issue for issue in jira.search_issues('text ~ "' + str(equipment.serial) + '" and status != closed')]
+    search_string = 'text ~ "' + str(equipment.serial) + '"'
+    if settings.JIRA_TICKETING['projects']:
+        search_string += ' AND project IN (' + settings.JIRA_TICKETING['projects'] + ')'
+    if not closed:
+        search_string += ' AND status != closed'
+    issues = list(jira.search_issues(search_string))
     for issue in issues:
         name = issue.key
         if str(issue.fields.status) == "closed":
             status = 'closed'
         else:
             status = 'open'
-        t = Ticket(state=status,
-                   url=settings.JIRA_TICKETING_URL+name,
-                   name=name)
-        t.save()
+        t, created = Ticket.objects.update_or_create(
+            name=name,
+            defaults={
+                'state': status,
+                'url': settings.JIRA_TICKETING['url']+name,
+            },
+        )
         equipment.ticket_set.add(t)
     return equipment
