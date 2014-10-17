servermon/servermon

commit ce5292fed61e0737491752bac643a72ecf4676a8

Fix bug in hwdoc's admin views
Unit tests unveiled a bug where if a authenticated but not authorized
user tried to access some hwdoc admin URLs a 500 error would be return
because no ModelAdmin instance would be created. Make sure a ModelAdmin
instance is always returned regardless of the rights of the user

servermon/hwdoc/admin.py
@@ -286,8 +286,6 @@ class EquipmentAdmin(admin.ModelAdmin):
             self.readonly_fields = ()
             ServerManagementInline.exclude = ()
             ServerManagementInline.readonly_fields = ()
-            return super(EquipmentAdmin, self).change_view(request,
-                    object_id, extra_context=extra_context)
 
         if request.user.has_perm('hwdoc.can_change_comment'):
             self.readonly_fields = ('serial', 'rack', 'unit', 'purpose',
@@ -295,8 +293,8 @@ class EquipmentAdmin(admin.ModelAdmin):
             ServerManagementInline.readonly_fields = ('hostname', 'method', 'mac')
             ServerManagementInline.exclude = ('username', 'password',
                     'license', 'raid_license',)
-            return super(EquipmentAdmin, self).change_view(request,
-                    object_id, extra_context=extra_context)
+        return super(EquipmentAdmin, self).change_view(request,
+                object_id, extra_context=extra_context)
 
     def changelist_view(self, request, extra_context=None):
         '''
@@ -306,15 +304,12 @@ class EquipmentAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             self.list_display = EquipmentAdmin.list_display
             self.list_editable = EquipmentAdmin.list_editable
-            return super(EquipmentAdmin, self).changelist_view(request,
-                    extra_context=extra_context)
         if request.user.has_perm('hwdoc.can_change_comment'):
             self.list_editable = ['comments',]
             self.list_display = ('allocation', 'model', 'serial',
                 'rack', 'unit', 'purpose', 'comments')
-
-            return super(EquipmentAdmin, self).changelist_view(request,
-                    extra_context=extra_context)
+        return super(EquipmentAdmin, self).changelist_view(request,
+                extra_context=extra_context)
 
     def has_change_permission(self, request, obj=None):
         '''

commit 39d94aa0e83f8edeab6afc33c0edb344adc2070f

Fix a bug with permissions in admin views
Turns out the order of the permission checking was wrong and a superuser
would not have full access. Fix this by reversing permission checking.
The regression was introduced in ce5292fe

servermon/hwdoc/admin.py
@@ -282,17 +282,16 @@ class EquipmentAdmin(admin.ModelAdmin):
         Overriding view to disallow updating readonly fields
         '''
 
-        if request.user.is_superuser:
-            self.readonly_fields = ()
-            ServerManagementInline.exclude = ()
-            ServerManagementInline.readonly_fields = ()
-
         if request.user.has_perm('hwdoc.can_change_comment'):
             self.readonly_fields = ('serial', 'rack', 'unit', 'purpose',
             'allocation', 'model', )
             ServerManagementInline.readonly_fields = ('hostname', 'method', 'mac')
             ServerManagementInline.exclude = ('username', 'password',
                     'license', 'raid_license',)
+        if request.user.is_superuser:
+            self.readonly_fields = ()
+            ServerManagementInline.exclude = ()
+            ServerManagementInline.readonly_fields = ()
         return super(EquipmentAdmin, self).change_view(request,
                 object_id, extra_context=extra_context)
 
@@ -301,13 +300,13 @@ class EquipmentAdmin(admin.ModelAdmin):
         Overriding view to disallow updating readonly fields
         '''
 
-        if request.user.is_superuser:
-            self.list_display = EquipmentAdmin.list_display
-            self.list_editable = EquipmentAdmin.list_editable
         if request.user.has_perm('hwdoc.can_change_comment'):
             self.list_editable = ['comments',]
             self.list_display = ('allocation', 'model', 'serial',
                 'rack', 'unit', 'purpose', 'comments')
+        if request.user.is_superuser:
+            self.list_display = EquipmentAdmin.list_display
+            self.list_editable = EquipmentAdmin.list_editable
         return super(EquipmentAdmin, self).changelist_view(request,
                 extra_context=extra_context)
 