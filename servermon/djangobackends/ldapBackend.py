servermon/servermon

commit 8af59df2b4d823c20901525b46a97b7d07241e78

Fix bug in ldapBackend with LDAP_AUTH_GROUP
If LDAP_AUTH_GROUP was not defined in settings, ldapBackend would
raise an Exception making authentication impossible. Fix it using the
canonical way of checking for dictionary key existence

servermon/djangobackends/ldapBackend.py
@@ -82,7 +82,7 @@ class ldapBackend:
                 user.last_name = result_data[0][1]['sn'][0]
                 user.is_staff = settings.LDAP_AUTH_IS_STAFF
                 user.is_superuser = False
-                if settings.LDAP_AUTH_GROUP:
+                if 'LDAP_AUTH_GROUP' in settings:
                     try:
                         g = Group.objects.get(name=settings.LDAP_AUTH_GROUP)
                         user.groups.add(g)

commit 64a4635512388ca9e5bfcf954ddcaec2f8982afb

Fix regression with settings and LDAP_AUTH_GROUP
Introduced in 8af59df, it was NOT the canonical way of checking

servermon/djangobackends/ldapBackend.py
@@ -78,7 +78,7 @@ class ldapBackend:
                 user = User.objects.create_user(username, result_data[0][1]['mail'][0], None)
                 user.is_staff = settings.LDAP_AUTH_IS_STAFF
                 user.is_superuser = False
-                if 'LDAP_AUTH_GROUP' in settings:
+                if hasattr(settings, 'LDAP_AUTH_GROUP'):
                     try:
                         g = Group.objects.get(name=settings.LDAP_AUTH_GROUP)
                         user.groups.add(g)