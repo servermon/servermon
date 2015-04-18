3a026b6f9bc520c954e61a1be532d196325113dc
Alexandros Kosiaris <akosiaris@gmail.com>
2015-04-18 14:24:53 +0300

    Amend manage.py to support django 1.6 and higher
    Amend the manage.py script to also support django 1.6 and greater versions

--- servermon/manage.py
+++ servermon/manage.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import os
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    # First try django < 1.6
+    try:
+        from django.core.management import execute_manager
+        try:
+            import settings # Assumed to be in the same directory.
+        except ImportError:
+            sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+            sys.exit(1)
+        execute_manager(settings)
+    except ImportError:
+        # Now django >= 1.6
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
