servermon/servermon

commit 79b2c1d28aa4913c0bdafbc1c6cb18beef4660e0

Make the calculate_empty_units function a Rack method
calculate_empty_units was a very good candidate for becoming a rack
method. Do it and adjust calling code. Also add a method to return the
number of all units in a rack (aka height)

servermon/hwdoc/views.py
@@ -257,9 +257,12 @@ def rack(request, rack_id):
     except (IndexError, RackPosition.DoesNotExist):
         rack.next = None
 
-    equipments = functions.search(str(rack.name))
+    equipments = rack.equipment_set.all()
     equipments = functions.populate_hostnames(equipments)
-    equipments = functions.calculate_empty_units(rack, equipments)
+    equipments = list(equipments)
+    equipments.extend(map(lambda x: Equipment(unit=x, rack=rack),
+        rack.get_empty_units()))
+    equipments = sorted(equipments, key=lambda eq: eq.unit, reverse=True)
 
     equipments = { 'hwdoc': equipments,
                    'hwdoc_ru': [e for e in equipments if e.unit > 0],
@@ -282,9 +285,13 @@ def rackrow(request, rackrow_id):
     rackrow = get_object_or_404(RackRow, pk=rackrow_id)
     racks = rackrow.rackposition_set.select_related()
     for rack in racks:
-        equipments = rack.rack.equipment_set.select_related('model__vendor',
-                                                            'model')
-        rack.equipments = functions.calculate_empty_units(rack.rack, equipments)
+        equipments = rack.rack.equipment_set.all().select_related(
+                'model__vendor',
+                'model')
+        equipments = list(equipments)
+        equipments.extend(map(lambda x: Equipment(unit=x, rack=rack.rack),
+            rack.rack.get_empty_units()))
+        equipments = sorted(equipments, key=lambda eq: eq.unit, reverse=True)
     return render(request, template, {
         'rackrow': rackrow,
         'racks': racks,

servermon/hwdoc/functions.py
@@ -162,29 +162,3 @@ def populate_hostnames(equipment_list):
         except:
             pass
     return equipment_list
-
-def calculate_empty_units(rack, equipment_list):
-    '''
-    Calculates the empty units in a rack and adds virtual equipments with the
-    only attribute being unit. The result is not meant to be correct QuerySet
-
-    @type  rack: Rack
-    @param rack: An Instance of Model Rack
-    @type  equipment_list: Queryset
-    @param equipment_list: A Django queryset containing equipment that indeed is
-    on the rack
-
-    @rtype: QuerySet
-    @return: A QuerySet with virtual equipments added to
-    '''
-
-    units = []
-    for z in ((x+w for w in xrange(y)) for x,y in equipment_list.values_list('unit', 'model__u')):
-        units.extend(z)
-    empty_units = set(rack.model.units) - set(sorted(units))
-    equipment_list = list(equipment_list)
-
-    for empty_unit in empty_units:
-        equipment_list.append(Equipment(unit=empty_unit, rack=rack))
-
-    return sorted(equipment_list, key=lambda eq: eq.unit, reverse=True)

servermon/hwdoc/models.py
@@ -199,6 +199,26 @@ class Rack(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
+    def get_empty_units(self, related=None):
+        '''
+        Calculates the empty units in a rack and returns them in a list
+
+        @type  self: Rack
+        @param self: An Instance of Model Rack
+
+        @rtype: list
+        @return: A list with empty units
+        '''
+
+        equipment_list = self.equipment_set.all()
+        if related:
+            equipment_list = equipment_list.select_related(*related)
+        units = []
+        for z in ((x+w for w in range(y)) for x,y in equipment_list.values_list('unit', 'model__u')):
+            units.extend(z)
+        empty_units = set(self.model.units) - set(sorted(units))
+        return empty_units
+
 class RackRow(models.Model):
     '''
     Racks in a row are a RackRow

servermon/hwdoc/tests.py
@@ -133,6 +133,9 @@ class EquipmentTestCase(unittest.TestCase):
     def test_if_servers_in_same_rack(self):
         self.assertEqual(self.server1.rack, self.server2.rack)
 
+    def test_rack_empty_units(self):
+        self.assertIsInstance(self.rack.get_empty_units(), set)
+
     def test_dummy_management_fuctions(self):
         self.assertTrue(self.management.power_on())
         self.assertTrue(self.management.power_off())

commit fb00561337f794999d045ad370f7259ff2f98803

Fix bug introduced in 79b2c1d
equipments need to be an attribute to rack

servermon/hwdoc/views.py
@@ -291,7 +291,7 @@ def rackrow(request, rackrow_id):
         equipments = list(equipments)
         equipments.extend(map(lambda x: Equipment(unit=x, rack=rack.rack),
             rack.rack.get_empty_units()))
-        equipments = sorted(equipments, key=lambda eq: eq.unit, reverse=True)
+        rack.equipments = sorted(equipments, key=lambda eq: eq.unit, reverse=True)
     return render(request, template, {
         'rackrow': rackrow,
         'racks': racks,