servermon/servermon

commit 7ec946f2da070f6dd2e21c0b8ce6b6e7ca233b6d

Present 0u equipments nicely in the frontend
Calculate 0u equipments in the views and use a nice CSS to present it
them to the user. Many tanks to zmousm for this. This closes #18

servermon/hwdoc/views.py
@@ -259,8 +259,11 @@ def rack(request, rack_id):
 
     equipments = functions.search(str(rack.name))
     equipments = functions.populate_hostnames(equipments)
+    equipments = functions.calculate_empty_units(rack, equipments)
 
-    equipments = { 'hwdoc': functions.calculate_empty_units(rack, equipments), }
+    equipments = { 'hwdoc': equipments,
+                   'hwdoc_ru': [e for e in equipments if e.unit > 0],
+                   'hwdoc_noru': [e for e in equipments if e.unit == 0], }
 
     return render(request, template, { 'rack': rack, 'equipments': equipments })
 

servermon/projectwide/templates/hwdoc_searchresults.html
@@ -1,5 +1,43 @@
 {% load i18n %}
 {% load url from future %}
+  <div id="equip-container" class="row-fluid">
+{% if results.hwdoc_noru %}
+  <div class="span2">
+  <table class="table table-striped table-rotated-270">
+    <tbody>
+    <tr>
+      <th><div><div>{% trans "Serial" %}</div></div></th>
+      {% for result in results.hwdoc_noru %}
+      {% if result.serial %}
+      <td><div><div><a href="{% url "hwdoc.views.equipment" result.pk %}">{{ result.serial }}</a></div></div></td>
+      {% endif %}
+      {% endfor %}
+    </tr>
+    <tr>
+      <th><div><div>{% trans "Model" %}</div></div></th>
+      {% for result in results.hwdoc_noru %}
+      <td><div><div>{{ result.model.vendor }}&nbsp;{{ result.model.name }}</div></div></td>
+      {% endfor %}
+    </tr>
+    <tr>
+      <th><div><div>{% trans "Rack" %}</div></div></th>
+      {% for result in results.hwdoc_noru %}
+      <td><div><div>{{ result.rack }}</div></div></td>
+      {% endfor %}
+    </tr>
+    <tr>
+      <th><div><div>{% trans "IPMI" %}</div></div></th>
+      {% for result in results.hwdoc_noru %}
+      <td><div><div><a href="https://{{ result.servermanagement.hostname }}">{{ result.servermanagement.hostname }}</a></div></div></td>
+      {% endfor %}
+    </tr>
+    </tbody>
+  </table>
+  </div>
+    <div class="span10">
+{% else %}
+    <div class="span12">
+{% endif %}
   <table class="table table-striped table-condensed sortable">
     <tbody>
     <tr>
@@ -10,13 +48,12 @@
       <th>{% trans "Front" %}</th>
       <th>{% trans "Interior" %}</th>
       <th>{% trans "Back" %}</th>
-      <th>{% trans "Orientation" %}</th>
       <th>{% trans "IPMI Hostname" %}</th>
       <th>{% trans "Project" %}</th>
       <th>{% trans "Tickets" %}</th>
       <th>{% trans "Hostname" %}</th>
     </tr>
-    {% for result in results.hwdoc %}
+    {% for result in results.hwdoc_ru %}
     {% if result.serial %}
         <tr>
           {% if result.ticket_set.all %}
@@ -29,17 +66,16 @@
           {% endif %}
           {% endif %}
           <td>{{ result.model.vendor }}&nbsp;{{ result.model.name }}</td>
-          <td>{% if result.rack %}{{ result.rack }}{% else %}&mdash;{% endif %}</td>
-          <td>{% if result.unit %}{% for unit in result.model.units %}{{ result.unit|add:unit|add:"-1"|stringformat:"02d" }}<br/>{% endfor %}{% else %}&mdash;{% endif %}</td>
-          <td class="centered">{% if result.rack_front %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
-          <td class="centered">{% if result.rack_interior %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
-          <td class="centered">{% if result.rack_back %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
-	  <td>{{ result.orientation }}</td>
-          <td>{% if result.servermanagement %}<a href="https://{{ result.servermanagement.hostname }}">{{ result.servermanagement.hostname }}</a>{% else %}&mdash;{% endif %}</td>
+          <td>{{ result.rack }}</td>
+	  <td>{% for unit in result.model.units %}{{ result.unit|add:unit|add:"-1"|stringformat:"02d" }}<br/>{% endfor %}</td>
+          <td class="centered">{% if result.model.rack_front %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
+          <td class="centered">{% if result.model.rack_interior %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
+          <td class="centered">{% if result.model.rack_back %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
+          <td><a href="https://{{ result.servermanagement.hostname }}">{{ result.servermanagement.hostname }}</a></td>
           <td>{% if result.allocation %}<a href="{% url "hwdoc.views.project" result.allocation.pk %}">{{ result.allocation.name }}</a>{% else %}&mdash;{% endif %}</td>
           <td>
           {% for ticket in result.ticket_set.all %}
-          <a href="{{ ticket.url }}">{% if ticket.closed %}<del>{{ ticket.name }}</del>{% else %}{{ ticket.name }}{% endif %}</a>
+          <a href="{{ ticket.url }}">{{ ticket.name }}</a>
           {% empty %}
           &mdash;
           </td>
@@ -60,7 +96,6 @@
       <td>&nbsp;</td>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
-      <td>&nbsp;</td>
     {% else %}
     <tr class="success">
       <td>&nbsp;</td>
@@ -74,10 +109,11 @@
       <td>&nbsp;</td>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
-      <td>&nbsp;</td>
     </tr>
     {% endif %}
     {% endif %}
     {% endfor %}
     </tbody>
   </table>
+  </div>
+  </div>

servermon/hwdoc/templates/rack.html
@@ -34,3 +34,6 @@
 </div>
 
 {% endblock %}
+{% block extrahead %}
+<link href="{{ MEDIA_URL }}rotation.css" rel="stylesheet" media="screen">
+{% endblock %}

servermon/static/rotation.css
@@ -1,0 +1,43 @@
+/* table.table-rotated-270 td:before, */
+/* table.table-rotated-270 th:before { */
+/*     content: ""; */
+/*     padding: 50% 0; */
+/* } */
+table.table-rotated-270 td,
+table.table-rotated-270 th {
+    vertical-align: middle;
+    text-align: center;
+}
+table.table-rotated-270 td > div,
+table.table-rotated-270 th > div {
+    display: inline-block;
+    overflow: hidden;
+    width: 1.5em;
+}
+table.table-rotated-270 td > div > div,
+table.table-rotated-270 th > div > div {
+    display: inline-block;
+    white-space: nowrap;
+    /* line-height: 1.5; */
+    -moz-transform: rotate(-90deg);  /* FF3.5+ */
+    -o-transform: rotate(-90deg);  /* Opera 10.5 */
+    -webkit-transform: rotate(-90deg);  /* Saf3.1+, Chrome */
+    filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=0.083);  /* IE6,IE7 */
+    -ms-filter: "progid:DXImageTransform.Microsoft.BasicImage(rotation=0.083)"; /* IE8 */
+    transform-origin: center;
+    writing-mode: bt-rl;
+    filter: flipv fliph;
+}
+table.table-rotated-270 td > div > div:after,
+table.table-rotated-270 th > div > div:after {
+    content: "";
+    display: block;
+    margin: -1.5em 0 100%;
+}
+/* table.table-rotated-270 td:before, */
+/* table.table-rotated-270 th:before { */
+/*     display: inline-block;/\* allow to vertical-center a box aside *\/ */
+/*     vertical-align: middle; */
+/*     margin: 0 -0.25em; */
+/*     padding-top: 100%; */
+/* } */

commit da641db6906a5c1a6851b0c9749aa023ae3337f0

Fix zero_u bugs introduced in 7ec946f2
The changes in 7ec946f2 introduced a bug where some views would not be
populating hwdoc_ru. Use hwdoc instead of hwdoc_ru everywhere for now
and rename hwdoc_noru to hwdoc_zero_u. The latter is proper for the
function. Also in 7ec946f2 a lot of HTML table elements were erroneously
removed. Add them back.

servermon/hwdoc/views.py
@@ -264,9 +264,8 @@ def rack(request, rack_id):
         rack.get_empty_units()))
     equipments = sorted(equipments, key=lambda eq: eq.unit, reverse=True)
 
-    equipments = { 'hwdoc': equipments,
-                   'hwdoc_ru': [e for e in equipments if e.unit > 0],
-                   'hwdoc_noru': [e for e in equipments if e.unit == 0], }
+    equipments = { 'hwdoc': [e for e in equipments if e.unit > 0],
+                   'hwdoc_zero_u': [e for e in equipments if e.unit == 0], }
 
     return render(request, template, { 'rack': rack, 'equipments': equipments })
 

servermon/projectwide/templates/hwdoc_searchresults.html
@@ -1,13 +1,13 @@
 {% load i18n %}
 {% load url from future %}
   <div id="equip-container" class="row-fluid">
-{% if results.hwdoc_noru %}
+{% if results.hwdoc_zero_u %}
   <div class="span2">
   <table class="table table-striped table-rotated-270">
     <tbody>
     <tr>
       <th><div><div>{% trans "Serial" %}</div></div></th>
-      {% for result in results.hwdoc_noru %}
+      {% for result in results.hwdoc_zero_u %}
       {% if result.serial %}
       <td><div><div><a href="{% url "hwdoc.views.equipment" result.pk %}">{{ result.serial }}</a></div></div></td>
       {% endif %}
@@ -15,19 +15,19 @@
     </tr>
     <tr>
       <th><div><div>{% trans "Model" %}</div></div></th>
-      {% for result in results.hwdoc_noru %}
+      {% for result in results.hwdoc_zero_u %}
       <td><div><div>{{ result.model.vendor }}&nbsp;{{ result.model.name }}</div></div></td>
       {% endfor %}
     </tr>
     <tr>
       <th><div><div>{% trans "Rack" %}</div></div></th>
-      {% for result in results.hwdoc_noru %}
+      {% for result in results.hwdoc_zero_u %}
       <td><div><div>{{ result.rack }}</div></div></td>
       {% endfor %}
     </tr>
     <tr>
       <th><div><div>{% trans "IPMI" %}</div></div></th>
-      {% for result in results.hwdoc_noru %}
+      {% for result in results.hwdoc_zero_u %}
       <td><div><div><a href="https://{{ result.servermanagement.hostname }}">{{ result.servermanagement.hostname }}</a></div></div></td>
       {% endfor %}
     </tr>
@@ -48,12 +48,13 @@
       <th>{% trans "Front" %}</th>
       <th>{% trans "Interior" %}</th>
       <th>{% trans "Back" %}</th>
+      <th>{% trans "Orientation" %}</th>
       <th>{% trans "IPMI Hostname" %}</th>
       <th>{% trans "Project" %}</th>
       <th>{% trans "Tickets" %}</th>
       <th>{% trans "Hostname" %}</th>
     </tr>
-    {% for result in results.hwdoc_ru %}
+    {% for result in results.hwdoc %}
     {% if result.serial %}
         <tr>
           {% if result.ticket_set.all %}
@@ -68,14 +69,15 @@
           <td>{{ result.model.vendor }}&nbsp;{{ result.model.name }}</td>
           <td>{{ result.rack }}</td>
 	  <td>{% for unit in result.model.units %}{{ result.unit|add:unit|add:"-1"|stringformat:"02d" }}<br/>{% endfor %}</td>
-          <td class="centered">{% if result.model.rack_front %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
-          <td class="centered">{% if result.model.rack_interior %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
-          <td class="centered">{% if result.model.rack_back %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
-          <td><a href="https://{{ result.servermanagement.hostname }}">{{ result.servermanagement.hostname }}</a></td>
+          <td class="centered">{% if result.rack_front %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
+          <td class="centered">{% if result.rack_interior %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
+          <td class="centered">{% if result.rack_back %}<i class="icon-ok-sign"></i>{% else %}&nbsp;{% endif %}</td>
+          <td>{{ result.orientation }}</td>
+          <td>{% if result.servermanagement %}<a href="https://{{ result.servermanagement.hostname }}">{{ result.servermanagement.hostname }}</a>{% else %}&mdash;{% endif %}</td>
           <td>{% if result.allocation %}<a href="{% url "hwdoc.views.project" result.allocation.pk %}">{{ result.allocation.name }}</a>{% else %}&mdash;{% endif %}</td>
           <td>
           {% for ticket in result.ticket_set.all %}
-          <a href="{{ ticket.url }}">{{ ticket.name }}</a>
+          <a href="{{ ticket.url }}">{% if ticket.closed %}<del>{{ ticket.name }}</del>{% else %}{{ ticket.name }}{% endif %}</a>
           {% empty %}
           &mdash;
           </td>
@@ -96,6 +98,7 @@
       <td>&nbsp;</td>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
+      <td>&nbsp;</td>
     {% else %}
     <tr class="success">
       <td>&nbsp;</td>
@@ -105,6 +108,7 @@
       <td class="centered">&nbsp;</td>
       <td class="centered">&nbsp;</td>
       <td class="centered">&nbsp;</td>
+      <td>&nbsp;</td>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
       <td>&nbsp;</td>